79ac6f9fe744d56100dcddc59befbb604c898cfb
Gabor Szarnyas <szarnyasg@gmail.com>
2016-11-27 23:20:54 -0500

    Rename Rete classes. Fixes #9

---
+++ src/main/scala/hu/bme/mit/ire/JoinNode.scala
@@ -0,0 +1,103 @@
+package hu.bme.mit.ire
+
+import scala.collection.mutable
+
+/**
+  * Created by wafle on 12/25/2015.
+  */
+class JoinNode(override val next: (ReteMessage) => Unit,
+               override val primarySelector: Vector[Any],
+               override val secondarySelector: Vector[Any]
+                ) extends JoinNodeImpl(primarySelector, secondarySelector) with SingleForwarder {
+}
+
+class NaturalJoinNode(override val next: (ReteMessage) => Unit, val selector: Vector[Any]
+                   ) extends JoinNodeImpl(selector,selector) with SingleForwarder {}
+
+class ParallelJoinNode(override val children: Vector[(ReteMessage) => Unit],
+                       override val primarySelector: Vector[Any],
+                       override val secondarySelector: Vector[Any],
+                       hashFunction: (TupleType) => Int = n => n.hashCode()
+                        ) extends JoinNodeImpl(primarySelector, secondarySelector) with ForkingForwarder {
+  override def forwardHashFunction(n: TupleType): Int = hashFunction(n)
+
+  override def forward(cs: ChangeSet) = super[ForkingForwarder].forward(cs)
+  override def forward(t: TerminatorMessage) = super[ForkingForwarder].forward(t)
+}
+
+abstract class JoinNodeImpl(val primarySelector: Vector[Any],
+                            val secondarySelector: Vector[Any])
+  extends BinaryNode {
+  val primaryValues = new mutable.HashMap[Vector[Any], mutable.Set[TupleType]]
+    with mutable.MultiMap[Vector[Any], TupleType]
+  val secondaryValues = new mutable.HashMap[Vector[Any], mutable.Set[TupleType]]
+    with mutable.MultiMap[Vector[Any], TupleType]
+
+  def onPrimary(changeSet: ChangeSet): Unit = {
+    val positive = changeSet.positive
+    val negative = changeSet.negative
+
+    val joinedPositive = for {
+      primary <- positive
+      key = primarySelector.map(i => primary(i))
+      if secondaryValues.contains(key)
+      secondary <- secondaryValues(key)
+    } yield primary ++ secondary.filterKeys( k => !primary.keySet.contains(k))
+
+    val joinedNegative = for {
+      primary <- negative
+      key = primarySelector.map(i => primary(i))
+      if secondaryValues.contains(key)
+      secondary <- secondaryValues(key)
+    } yield primary ++ secondary.filterKeys( k => !primary.keySet.contains(k))
+
+    forward(ChangeSet(joinedPositive, joinedNegative))
+    positive.foreach(
+      m => {
+        val key = primarySelector.map(i => m(i))
+        primaryValues.addBinding(key, m)
+      }
+    )
+    negative.foreach(
+      m => {
+        val key = primarySelector.map(i => m(i))
+        primaryValues.removeBinding(key, m)
+      }
+    )
+  }
+
+  def onSecondary(changeSet: ChangeSet): Unit = {
+    val positive = changeSet.positive
+    val negative = changeSet.negative
+
+    val joinedPositive = for {
+      secondary <- positive
+      key = secondarySelector.map(i => secondary(i))
+      if primaryValues.contains(key)
+      primary <- primaryValues(key)
+    } yield primary ++ secondary.filterKeys( k => !primary.keySet.contains(k))
+
+
+    val joinedNegative = for {
+      secondary <- negative
+      key = secondarySelector.map(i => secondary(i))
+      if primaryValues.contains(key)
+      primary <- primaryValues(key)
+    } yield primary ++ secondary.filterKeys( k => !primary.keySet.contains(k))
+
+    forward(ChangeSet(joinedPositive, joinedNegative))
+
+    positive.foreach(
+      m => {
+        val key = secondarySelector.map(i => m(i))
+        secondaryValues.addBinding(key, m) //must be used with multimaps
+      }
+    )
+    negative.foreach(
+      m => {
+        val key = secondarySelector.map(i => m(i))
+        secondaryValues.removeBinding(key, m)
+      }
+    )
+  }
+}
---
+++ src/main/scala/hu/bme/mit/ire/UnaryNode.scala
@@ -0,0 +1,29 @@
+package hu.bme.mit.ire
+
+import akka.actor.{Actor, Stash}
+
+/**
+  * Created by wafle on 12/25/2015.
+  */
+abstract class UnaryNode(val expectedTerminatorCount: Int = 1) extends Actor with Forwarder with Stash with TerminatorHandler {
+  val log = context.system.log
+  val name = self.path.name
+  def onChangeSet(changeSet: ChangeSet)
+
+  override def receive: Actor.Receive = {
+    case pause: Pause => context.become({
+      case resume: Resume => {
+        if (resume.messageID == pause.messageID) {
+          context.unbecome()
+          unstashAll()
+        } else stash()
+      }
+      case terminator: TerminatorMessage => handleTerminator(terminator)
+      case _ => stash()
+    })
+    case changeSet: ChangeSet => onChangeSet(changeSet)
+    case terminator: TerminatorMessage => handleTerminator(terminator)
+    case Primary | Secondary =>
+      throw new UnsupportedOperationException(s"$name received Beta-wrapped message")
+  }
+}
