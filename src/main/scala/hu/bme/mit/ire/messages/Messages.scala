9ff19fbd504e9b6e32c9286bbdc1d564a15f4541
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-04 13:41:49 -0500

    Refactor code into packages

---
+++ src/main/scala/hu/bme/mit/ire/messages/Messages.scala
@@ -0,0 +1,24 @@
+package hu.bme.mit.ire.messages
+
+import akka.actor.ActorRef
+import hu.bme.mit.ire.datatypes.TupleType
+
+import scala.concurrent.Promise
+
+/**
+  * Created by wafle on 12/25/2015.
+  */
+class ReteMessage() {}
+
+case class ChangeSet(positive: Vector[TupleType] = Vector(), negative: Vector[TupleType] = Vector())
+  extends ReteMessage()
+
+case class ExpectMoreTerminators(id: Int, inputs: Iterable[ReteMessage => Unit])
+
+case class TerminatorMessage(terminatorID: Int, messageID: Int, route: List[ActorRef] = List.empty) extends ReteMessage()
+
+case class ExpectTerminator(terminatorID: Int, messageID: Int, promise: Promise[Set[TupleType]]) extends ReteMessage()
+
+case class Pause(messageID: Int) extends ReteMessage()
+
+case class Resume(messageID: Int) extends ReteMessage()
